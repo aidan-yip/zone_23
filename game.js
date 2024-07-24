6aefc2a3a3e0a841d11da604f0269c214dfb1db2
Aidan Y <jamesayipt6@gmail.com>
2024-07-23 18:52:59 -0700

    Added stars

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-// TODO: document
+  TODO: document
 
 kaplay({
   background: [15, 15, 133],
 });
 
-const objs = ["ghosty", "flag", "bag"];
+const objs = ["ghosty", "flag", "bag", "stars"];
 
 for (const obj of objs) {
   loadSprite(obj, `./sprites/${obj}.png`);
@@ -102,26 +102,26 @@ scene("battle", () => {
     }
   });
 
-  // 	add([
-  // 		sprite("stars"),
-  // 		scale(width() / 240, height() / 240),
-  // 		pos(0, 0),
-  // 		"stars",
-  // 	])
+    	add([
+    		sprite("stars"),
+    		scale(width() / 240, height() / 240),
+    		pos(0, 0),
+    		"stars",
+    	])
 
-  // 	add([
-  // 		sprite("stars"),
-  // 		scale(width() / 240, height() / 240),
-  // 		pos(0, -height()),
-  // 		"stars",
-  // 	])
+    	add([
+    		sprite("stars"),
+    		scale(width() / 240, height() / 240),
+    		pos(0, -height()),
+    		"stars",
+    	])
 
-  // 	onUpdate("stars", (r) => {
-  // 		r.move(0, STAR_SPEED * (insaneMode ? 10 : 1))
-  // 		if (r.pos.y >= height()) {
-  // 			r.pos.y -= height() * 2
-  // 		}
-  // 	})
+    	onUpdate("stars", (r) => {
+    		r.move(0, STAR_SPEED * (insaneMode ? 10 : 1))
+    		if (r.pos.y >= height()) {
+    			r.pos.y -= height() * 2
+    		}
+    	})
 
   const player = add([
     sprite("bag"),
